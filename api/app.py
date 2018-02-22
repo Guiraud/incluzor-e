ca5121de5d35821e29c7ed875a6bf822f42ea61e
Guy Lifshitz <guylifshitz@gmail.com>
2018-02-22 17:07:00 +0100

    Add new lexique with point médian writing.

--- api/app.py
+++ api/app.py
@@ -7,6 +7,7 @@ from parser import Parser
 from pycorenlp import StanfordCoreNLP
 from io import StringIO
 from nltk.tree import Tree
+import re
 
 # Setup Flask
 app = FlaskAPI(__name__)
@@ -23,36 +24,53 @@ def parse_sentence():
     if request.method == 'GET':
         text = request.args.get('input')
 
-        parse_output = nlp.annotate(text, properties={
-            'annotators': 'tokenize,ssplit,pos,depparse,parse',
-            'outputFormat': 'json'
-        })
-
-        parsed_sentences = [s['parse'] for s in parse_output['sentences']]
-
         all_output = ""
         parsed_output = []
-        for parsed_sentence in parsed_sentences:
 
-            # Get the tree diagram
-            tree_stream = StringIO()
-            Tree.fromstring(parsed_sentence).pretty_print(stream=tree_stream)
-            tree_string = tree_stream.getvalue()
+        # sentences = re.split(r'(\s+)', text)
+        # sentences = re.split('.', text)
+        # print(sentences)
+        # sentences = [text]
+        # sentences = text.split(["\n", "."])
+        # re.split('\n|,',str)
+        sentences = re.split('(\n)', text)
+        # sentences = re.split('; |\.|\*|\n', text)
+        # sentences = [text]
 
-            converted_sentence = parser.parse(parsed_sentence)
-            parsed_output.append({
-                "parsed": parsed_sentence,
-                "converted": converted_sentence,
-                "tree": tree_string
+        for sentence in sentences:
+            print("Parse sentence: ", sentence)
+            if len(sentence) > 1:
+                parse_output = nlp.annotate(sentence, properties={
+                    'annotators': 'tokenize,ssplit,pos,depparse,parse',
+                    'outputFormat': 'json'
                 })
-            all_output += converted_sentence
 
-        all_output = all_output.replace(' .', '. ')
+                parsed_sentences = [s['parse'] for s in parse_output['sentences']]
 
-        return {"converted_text": all_output, "parsed_sentences": parsed_output}
+                for parsed_sentence in parsed_sentences:
+
+                    # Get the tree diagram
+                    tree_stream = StringIO()
+                    Tree.fromstring(parsed_sentence).pretty_print(stream=tree_stream)
+                    tree_string = tree_stream.getvalue()
+
+                    converted_sentence = parser.parse(parsed_sentence)
+                    parsed_output.append({
+                        "parsed": parsed_sentence,
+                        "converted": converted_sentence,
+                        "tree": tree_string
+                        })
+                    all_output += converted_sentence
+            else:
+                all_output += sentence
+
+        all_output = all_output.replace('.', '. ')
+        html_output = all_output
+
+        return {"converted_text": all_output, "converted_text_html": html_output, "parsed_sentences": parsed_output}
     return {"Status": "Fail"}
 
 
 if __name__ == '__main__':
     app.debug = True
-    app.run(host='0.0.0.0',port=5005)
+    app.run(host='0.0.0.0', port=5005)
--- api/parser.py
+++ api/parser.py
@@ -17,8 +17,8 @@ class Parser():
 
     def setup_parser(self):
         # liste des noms
-        self.noms_liste = self.load_csv_url("http://test.incluzor.fr/data/lexique-v0.1.csv", sep=";")
-
+        self.noms_liste = self.load_csv_url("http://test.incluzor.fr/data/lexique_numerique_fusion_guy.tsv", sep="\t")
+        self.noms_liste["ignore"] = ""
         # lists des régularités des adjectifs
         self.adjectifs_reg = self.load_csv_url("http://test.incluzor.fr/data/adjectivesv-0.1.csv")
 
@@ -30,7 +30,6 @@ class Parser():
     def tag_parent(self, tree, plural):
         parent = tree.parent()
         if parent.label() == "NP":
-            print("NP")
             if plural:
                 parent.set_label("NP-BiPlur")
             else:
@@ -39,22 +38,36 @@ class Parser():
     def find_nc(self, tree):
         if tree.height() == 2:
         # TODO handle case of both sing and plur
-        # TODO handle PRO as their own thing        https://fr.wikipedia.org/wiki/Pronom_ind%C3%A9fini#Le_pronom_%C2%AB_nul_%C2%BB
+        # TODO handle PRO as their own thing
+        #      see: https://fr.wikipedia.org/wiki/Pronom_ind%C3%A9fini#Le_pronom_%C2%AB_nul_%C2%BB
             if tree.label() == "NC" or tree.label() == "PRO":
                 word = tree[0].lower().strip()
+
+                # CHECK FOR SINGULAR
                 noun_matches = self.noms_liste[(self.noms_liste["Masc Singulier"] == word) & (self.noms_liste["ignore"] != "x")]
+
                 if noun_matches.shape[0] > 1:
                     raise Exception("noun_matches.shape[0] > 1")
                 elif noun_matches.shape[0] > 0:
-                    forme_bigenre = noun_matches["Masc Singulier"].iloc[0] + "/" + noun_matches["Fem Singulier"].iloc[0]
+                    if noun_matches["Masc Singulier"].iloc[0] == noun_matches["Fem Singulier"].iloc[0]:
+                        forme_bigenre = noun_matches["Masc Singulier"].iloc[0]
+                    else:
+                        forme_bigenre = noun_matches["Incl sing V1"].iloc[0]
+
                     self.tag_parent(tree, False)
                     tree.__setitem__(0, forme_bigenre)
 
+                # CHECK FOR PLURAL
                 noun_matches = self.noms_liste[(self.noms_liste["Masc Pluriel"] == word) & (self.noms_liste["ignore"] != "x")]
+
                 if noun_matches.shape[0] > 1:
                     raise Exception("noun_matches.shape[0] > 1")
                 elif noun_matches.shape[0] > 0:
-                    forme_bigenre = noun_matches["Masc Pluriel"].iloc[0] + "/" + noun_matches["Fem Pluriel"].iloc[0]
+                    if noun_matches["Masc Pluriel"].iloc[0] == noun_matches["Fem Pluriel"].iloc[0]:
+                        forme_bigenre = noun_matches["Masc Pluriel"].iloc[0]
+                    else:
+                        forme_bigenre = noun_matches["Incl pl V1"].iloc[0]
+
                     self.tag_parent(tree, True)
                     tree.__setitem__(0, forme_bigenre)
 
@@ -62,68 +75,25 @@ class Parser():
             if type(subtree) == nltk.tree.ParentedTree:
                 self.find_nc(subtree)
 
-
     def replace_det(self, det, plural):
         try:
             return self.determinants_liste[self.determinants_liste["masc"] == det]["bigenre"].iloc[0]
         except:
             return "[DET]"
-        # return df[]
-        # for idx, row in
-        # if det == "le":
-        #     return "le.la"
-        # elif det == "les":
-        #     return "les"
-        # elif det == "du":
-        #     return "du.de la"
-        # elif det == "tous":
-        #     return "tous."
-        # else:
-        #     return "[DET]"
-        # return det
 
-    # private String addAdjectiveEnding(String adjective, boolean plural) {
-
-    #     String outWord = adjective;
-
-    #     // Check each adjective ending rule for a match
-    #     // and replace it with the correct ending
-    #     for (HashMap<String, String> adjEnding : adjectivesEndings) {
-
-    #         String mascEnding = adjEnding.get("masc_sing").toLowerCase().trim();
-    #         if (plural) {
-    #             mascEnding = adjEnding.get("masc_plur").toLowerCase().trim();
-    #         }
-
-    #         if (adjective.toLowerCase().endsWith(mascEnding)) {
-    #             outWord = adjective.substring(0, adjective.length() - mascEnding.length());
-    #             if (plural) {
-    #                 outWord += adjEnding.get("inc_plur").toLowerCase().trim();
-    #             } else {
-    #                 outWord += adjEnding.get("inc_sing").toLowerCase().trim();
-    #             }
-    #         }
-
-    #     }
-
-    #     return outWord;
-    # }
     def replace_adj(self, adj, plural):
         for idx, row in self.adjectifs_reg.iterrows():
             if plural:
-                masc_ending = row["masc_sing"]
-            else:
                 masc_ending = row["masc_plur"]
-
-            print("masc_ending", masc_ending)
-            print("adj", adj)
+            else:
+                masc_ending = row["masc_sing"]
 
             if adj.endswith(masc_ending):
                 out_adj = adj[0:len(adj) - len(masc_ending)]
                 out_adj += row.get("inc_plur").lower().strip()
                 return out_adj
 
-        return "[ADJ]"
+        return out_adj
 
     def replace_accords(self, tree, inclusive, plural):
         if tree.label() == "NP-BiSing":
@@ -147,13 +117,16 @@ class Parser():
                 self.replace_accords(subtree, inclusive, plural)
 
     def parse(self, sentence):
+
         sentence = ParentedTree.fromstring(sentence)
-        # sentence = ParentedTree.convert(sentence)
+
+        sentence.pretty_print()
+
         self.find_nc(sentence)
         self.replace_accords(sentence, False, False)
 
-        out = " ".join(sentence.leaves())
-
         sentence.pretty_print()
+
+        out = " ".join(sentence.leaves())
 
         return out
